a46d5d7902dbf9848341402ff7482b98ca4e7fa4
dudeamax99 <dudeamax99@protonmail.com>
2021-01-28 10:15:46 -0500

    Add .deb building to nightly builds for jailbroken devices

--- Misc/scripts/package.sh
+++ Misc/scripts/package.sh
@@ -4,6 +4,7 @@ XCARCHIVE="$(pwd)/$1"
 PLATFORM="$2"
 PROJ_DIR=`pwd`
 PACKAGE_NAME="$3"
+PACKAGE_VER="${PACKAGE_NAME:21:4}"
 BIN_PKG_PATH=`find "${XCARCHIVE}" -name '*.app' -print0`
 TMPDIR="$PROJ_DIR/build"
 LICENSE_FILE="$PROJ_DIR/LICENSE"
@@ -20,6 +21,22 @@ package_iOS() {
     cp -r "${BIN_PKG_PATH}" "Payload/${APPNAME}.app"
     zip -9 -r "${PACKAGE_NAME}.ipa" Payload
     rm -rf Payload
+    
+    echo "[*] Generating .deb archive..."
+    
+    mkdir -p deb/DEBIAN
+    mkdir -p deb/Applications
+    cat > deb/DEBIAN/control <<EOF 
+    Package: NineAnimator
+    Maintainer: Marcus Zhou <marcus_zhou@icloud.com>
+    Architecture: iphoneos-arm
+    Version: ${PACKAGE_VER}
+    Homepage: https://9ani.app
+    Description: A simple yet elegant way of waching anime on your favorite anime websites. NineAnimator is a free and open source anime watching app for iOS and macOS. GPLv3 Licensed.
+    EOF
+    cp -r "${BIN_PKG_PATH}" "deb/Applications/${APPNAME}.app"
+    dpkg-deb --build deb ${PACKAGE_NAME}.deb
+    rm -rf deb
     
     echo "[*] Generating .dSYM archive..."
     
